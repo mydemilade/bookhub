51ecde3d746aecb0f4f7972e6666727296722f98
mydemilade <ayunoluwademilade@gmail.com>
2023-10-26 00:17:59 +0100

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -22,6 +22,17 @@ const Navbar = () => {
             }} />
           </button>
       </div>
+
+      <div className={toggleMenu ? "navbar-collapse show-navbar-collapse" : "navbar-collapse"}>
+          <ul className = "navbar-nav">
+            <li className='nav-item'>
+              <Link to = "book" className='nav-link text-uppercase text-white fs-22 fw-6 ls-1'>Home</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to = "about" className='nav-link text-uppercase text-white fs-22 fw-6 ls-1'>about</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   )
